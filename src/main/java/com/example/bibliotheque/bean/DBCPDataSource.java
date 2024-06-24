fa518392482d4d91df49dbfd3b3d2ffa9e157cbb
Ismael-Raveloson <120553853+Ismael-Raveloson@users.noreply.github.com>
2024-06-24 10:37:57 +0300

    changement connection

--- src/main/java/com/example/bibliotheque/bean/DBCPDataSource.java
+++ src/main/java/com/example/bibliotheque/bean/DBCPDataSource.java
@@ -10,9 +10,15 @@ public class DBCPDataSource {
     private static BasicDataSource ds = new BasicDataSource();
     
     static {
-        ds.setUrl("jdbc:postgresql://localhost:5432/bibliotheque");
+        // ds.setUrl("jdbc:postgresql://localhost:5432/bibliotheque");
+        // ds.setUsername("postgres");
+        // ds.setPassword("root");
+        // ds.setMinIdle(5);
+        // ds.setMaxIdle(10);
+        // ds.setMaxOpenPreparedStatements(100);
+        ds.setUrl("postgresql://postgres:VnyKHnoptbwhOCQbbtNieKEynFbhQnOn@viaduct.proxy.rlwy.net:27630/railway");
         ds.setUsername("postgres");
-        ds.setPassword("root");
+        ds.setPassword("VnyKHnoptbwhOCQbbtNieKEynFbhQnOn");
         ds.setMinIdle(5);
         ds.setMaxIdle(10);
         ds.setMaxOpenPreparedStatements(100);
